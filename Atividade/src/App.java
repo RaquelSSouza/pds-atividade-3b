25c22ed1fb5fdd06c6bb3ca580d5d643a41ca921
RAQUEL SILVA SOUZA <20211064010025@ifrn.local>
2024-12-16 17:25:13 -0300

    projeto pronto

---
+++ Atividade/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        // Criando uma instância de Pessoa para cada estuantes
+        Pessoa pessoa = new Pessoa("Raquel", 19);
+
+        // Exibindo as informações dos estudantes
+        pessoa.exibirInformacoes();
+    }
+}
